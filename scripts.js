a2de0683d8f4d4beb69c15bc6dc905b46c84908f
thinkocapo <thinkocapo@gmail.com>
2018-04-22 22:17:45 -0400

    implemented. cleaning

--- scripts.js
+++ scripts.js
@@ -1,73 +1,24 @@
 import Web3 from 'web3'
 import { createRawTransaction, createSignedSerializedTransaction, hex } from './utils'
-
-const eosContractAddress = '0xd0a6E6C54DbC68Db5db3A091B171A77407Ff7ccf'
-const eosContractABI = [{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"address"}],"name":"claimed","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"owner_","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"time","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint128"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"foundersAllocation","outputs":[{"name":"","type":"uint128"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"day","type":"uint256"}],"name":"claim","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"foundersKey","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"address"}],"name":"userBuys","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"day","type":"uint256"}],"name":"createOnDay","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"freeze","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"keys","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"startTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"authority_","type":"address"}],"name":"setAuthority","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"dailyTotals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"buy","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"openTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"EOS","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"today","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"authority","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"eos","type":"address"}],"name":"initialize","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"createFirstDay","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"claimAll","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"timestamp","type":"uint256"}],"name":"dayFor","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"day","type":"uint256"},{"name":"limit","type":"uint256"}],"name":"buyWithLimit","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"collect","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numberOfDays","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"string"}],"name":"register","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"createPerDay","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_numberOfDays","type":"uint256"},{"name":"_totalSupply","type":"uint128"},{"name":"_openTime","type":"uint256"},{"name":"_startTime","type":"uint256"},{"name":"_foundersAllocation","type":"uint128"},{"name":"_foundersKey","type":"string"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"window","type":"uint256"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"LogBuy","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"window","type":"uint256"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"LogClaim","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"key","type":"string"}],"name":"LogRegister","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"LogCollect","type":"event"},{"anonymous":false,"inputs":[],"name":"LogFreeze","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"authority","type":"address"}],"name":"LogSetAuthority","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"}],"name":"LogSetOwner","type":"event"}] // should NOT be a string or will throw error in web3js/contracts.js
-let webThree = new Web3
+// web3.eth.getTransaction(transactionHash [, callback])
+// web3.eth.getTransactionReceipt(hash [, callback])
 
 
 export async function sendEther (web3, ether, recipient) {
-  //const rawTransaction = await createRawTransaction(web3, ether, recipient)
-  //console.log('RAWTRANSACTION', rawTransaction)
-  return createRawTransaction(web3, ether, recipient)
-  .then(rawTransaction => {
-    const transactionSignedSerialized = createSignedSerializedTransaction(rawTransaction, process.env.privateKey)
-    
-    console.log('\n=== EARLY RETURN - DONE - DO NOT RUN web3.eth.sendRawTransaction ===')
-    return // remove once you've confirmed everything in the loggers looks good
-    // Test before merge Master - in v1.0 it was sendSignedTransaction, which was successfully tested
-    web3.eth.sendRawTransaction('0x' + transactionSignedSerialized.toString('hex'))
-      .then((err, result) => {
-        if (err) { console.log('======= err =======\n', JSON.stringify(err,null,4))
-        } else { 
-          console.log('======= result =======\n', JSON.stringify(result,null,4))
-          // web3.eth.getTransaction(transactionHash [, callback])
-          // web3.eth.getTransactionReceipt(hash [, callback])
-        }
-      })
-  })
-}
+  const rawTransaction = await createRawTransaction(web3, ether, recipient)
+  const transactionSignedSerialized = createSignedSerializedTransaction(rawTransaction, process.env.privateKey)
+
+  console.log('\n=== EARLY RETURN - DONE - DO NOT RUN web3.eth.sendRawTransaction ===')
   
-/**
- * TRADING ARBITRAGE BOT - make one method that performs the following
- * 1. checks if their are new EOS tokens to claim
- * 2. runs claimAll and exchange workflow
- * 3. if no new EOS token available, then sendEther() to eosContractAddress (Crowdsale)
- */
-export function  claimEos (web3) {
-  const eosContractInstance = new web3.eth.Contract(eosContractABI, eosContractAddress);
+  // Remove once you've confirmed everything in the loggers looks good
+  return 
+  
+  // 4/22 sendRawTransaction hasn't been tested yet - in v1.0 it was sendSignedTransaction, which was successfully tested
+  web3.eth.sendRawTransaction('0x' + transactionSignedSerialized.toString('hex'))
+    .then((err, result) => {
+      if (err) { console.log('======= err =======\n', JSON.stringify(err,null,4))
+      } else { 
+        console.log('======= result =======\n', JSON.stringify(result,null,4))
+      }
+    })
 }
-export function tradeEOSforEther () {
-  // exchange api - send eos to exchange
-  // exchange api - exchange eos for eth
-  // exchange api - send eth back to default account address
-  // log details on what fees/money was made
-}
-
-/* MISC */
-export async function filterWatches (web3) {
-  // ERROR web3.eth.filter is not a function... isn't there an newer syntax if you check the wiki doc?
-  // var filter = web3.eth.filter('pending'); // .Filter
-  // var filter = web3.eth.subscribe('pendingTransactions', function (err, res) {
-  //   console.log('\n -- SUBSCRIBE err --\n', err) // current provider doesnt support subscriptions: HTTPProvider
-  //   console.log('\n -- SUBSCRIBE res --\n', res)
-  // });
-  // filter.watch(function(error, result) {
-  //   if (!error) {
-  //     web3.eth.getTransaction(result, function(error, data) {
-  //       if (!error) $("#newTxs tr:first").after(''+data.from+''+data.to+' '+web3.fromWei(data.value,'ether').toString()+' ETH ');
-  //     });
-  //   }
-  // });
-  //Result...
-  //checkout latest transactions below, it might take a second to load as it is waiting for incoming txs
-}
-export async function getBalance (web3) {
-    web3.eth.getBalance("0x7602aCd0a747332Ce638a0b9f6d7532767303C8F").then((res, err) => {})
-}
-export async function getBlock (web3) {
-  return web3.eth.getBlock(5100000).then((res, err) => {})
-}
-
-
-
